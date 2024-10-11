e9d113485e94c16296de3a55633a037a17188d3c
MrHope03 <nitishsgk@gmail.com>
2024-10-11 18:30:48 +0530

    Added colorscheme lua to neovim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+
+}
