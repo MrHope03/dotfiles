e2e9b0549061a521c6ac5f88b97b44259da83f73
MrHope03 <nitishsgk@gmail.com>
2024-10-11 17:55:37 +0530

    Added ohmyposh dotfile

---
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
